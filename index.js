c57cf36912c0fab19868c0eb9334feba2312139e
Fero <115424882+F3ro0@users.noreply.github.com>
2022-12-13 13:03:29 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,216 @@
+let countEl1 = document.getElementById("count-el1")
+
+let count1 = 0;
+
+function increment1(){
+  count1 += 1
+  countEl1.textContent = count1
+};
+
+function decrement1() {if (count1 === 0) {
+  alert("You can't go to negative values!")
+} else {
+  count1 -= 1
+  countEl1.textContent = count1
+};
+};
+
+
+let countEl2 = document.getElementById("count-el2")
+
+let count2 = 0;
+
+function increment2(){
+  count2 += 1
+  countEl2.textContent = count2
+};
+
+function decrement2() {if (count2 === 0) {
+  alert("You can't go to negative values!")
+} else {
+  count2 -= 1
+  countEl2.textContent = count2
+};
+};
+
+
+let countEl3 = document.getElementById("count-el3")
+
+let count3 = 0;
+
+function increment3(){
+  count3 += 1
+  countEl3.textContent = count3
+};
+
+function decrement3() {if (count3 === 0) {
+  alert("You can't go to negative values!")
+} else {
+  count3 -= 1
+  countEl3.textContent = count3
+};
+};
+
+
+let countEl4 = document.getElementById("count-el4")
+
+let count4 = 0;
+
+function increment4(){
+  count4 += 1
+  countEl4.textContent = count4
+};
+
+function decrement4() {if (count4 === 0) {
+  alert("You can't go to negative values!")
+} else {
+  count4 -= 1
+  countEl4.textContent = count4
+};
+};
+
+
+let countEl5 = document.getElementById("count-el5")
+
+let count5 = 0;
+
+function increment5(){
+  count5 += 1
+  countEl5.textContent = count5
+};
+
+function decrement5() {if (count5 === 0) {
+  alert("You can't go to negative values!")
+} else {
+  count5 -= 1
+  countEl5.textContent = count5
+};
+};
+
+
+let countEl6 = document.getElementById("count-el6")
+
+let count6 = 0;
+
+function increment6(){
+  count6 += 1
+  countEl6.textContent = count6
+};
+
+function decrement6() {if (count6 === 0) {
+  alert("You can't go to negative values!")
+} else {
+  count6 -= 1
+  countEl6.textContent = count6
+};
+};
+
+
+let countEl7 = document.getElementById("count-el7")
+
+let count7 = 0;
+
+function increment7(){
+  count7 += 1
+  countEl7.textContent = count7
+};
+
+function decrement7() {if (count7 === 0) {
+  alert("You can't go to negative values!")
+} else {
+  count7 -= 1
+  countEl7.textContent = count7
+};
+};
+
+
+let countEl8 = document.getElementById("count-el8")
+
+let count8 = 0;
+
+function increment8(){
+  count8 += 1
+  countEl8.textContent = count8
+};
+
+function decrement8() {if (count8 === 0) {
+  alert("You can't go to negative values!")
+} else {
+  count8 -= 1
+  countEl8.textContent = count8
+};
+};
+
+
+let countEl9 = document.getElementById("count-el9")
+
+let count9 = 0;
+
+function increment9(){
+  count9 += 1
+  countEl9.textContent = count9
+};
+
+function decrement9() {if (count9 === 0) {
+  alert("You can't go to negative values!")
+} else {
+  count9 -= 1
+  countEl9.textContent = count9
+};
+};
+
+
+let countEl10 = document.getElementById("count-el10")
+
+let count10 = 0;
+
+function increment10(){
+  count10 += 1
+  countEl10.textContent = count10
+};
+
+function decrement10() {if (count10 === 0) {
+  alert("You can't go to negative values!")
+} else {
+  count10 -= 1
+  countEl10.textContent = count10
+};
+};
+
+
+let countEl11 = document.getElementById("count-el11")
+
+let count11 = 0;
+
+function increment11(){
+  count11 += 1
+  countEl11.textContent = count11
+};
+
+function decrement11() {if (count11 === 0) {
+  alert("You can't go to negative values!")
+} else {
+  count11 -= 1
+  countEl11.textContent = count11
+};
+};
+
+
+let countEl12 = document.getElementById("count-el12")
+
+let count12 = 0;
+
+function increment12(){
+  count12 += 1
+  countEl12.textContent = count12
+};
+
+function decrement12() {if (count12 === 0) {
+  alert("You can't go to negative values!")
+} else {
+  count12 -= 1
+  countEl12.textContent = count12
+};
+};
+
+
